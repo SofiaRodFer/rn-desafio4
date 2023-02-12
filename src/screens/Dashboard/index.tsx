35aec18cd87a2997e86adcb39a15ebcb7b45489b
Sofia <sofiarodrigues45@gmail.com>
2023-02-12 19:54:32 -0300

    corrigindo erros

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -69,7 +69,7 @@ export function Dashboard() {
             <InputButton
               testID="input-button"
               onPress={handleAddRepository}
-              disabled={!!inputText}
+              disabled={inputText? false : true}
             >
               <Icon name="search" size={20} />
             </InputButton>
